3e93736494b8ff8067177f61dd21155f030fd49a
Ruben Gees <rubengees7@gmail.com>
2023-12-03 00:03:57 +0100

    Use biome instead of prettier and eslint

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -26,20 +26,20 @@ export function part2(input: string): string {
       )
     )
     .filter((it) => {
-      const [_, height, heightUnit] = it["hgt"]?.match("^(\\d+)(\\w+)$") ?? []
+      const [_, height, heightUnit] = it.hgt?.match("^(\\d+)(\\w+)$") ?? []
 
       return (
-        +it["byr"] >= 1920 &&
-        +it["byr"] <= 2002 &&
-        +it["iyr"] >= 2010 &&
-        +it["iyr"] <= 2020 &&
-        +it["eyr"] >= 2020 &&
-        +it["eyr"] <= 2030 &&
+        +it.byr >= 1920 &&
+        +it.byr <= 2002 &&
+        +it.iyr >= 2010 &&
+        +it.iyr <= 2020 &&
+        +it.eyr >= 2020 &&
+        +it.eyr <= 2030 &&
         ((heightUnit === "cm" && +height >= 150 && +height <= 193) ||
           (heightUnit === "in" && +height >= 59 && +height <= 76)) &&
-        (it["hcl"]?.match("^#[0-9a-f]{6}$") ?? false) &&
-        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(it["ecl"]) &&
-        (it["pid"]?.match("^\\d{9}$") ?? false)
+        (it.hcl?.match("^#[0-9a-f]{6}$") ?? false) &&
+        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(it.ecl) &&
+        (it.pid?.match("^\\d{9}$") ?? false)
       )
     })
     .length.toString()
